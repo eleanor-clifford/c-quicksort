e5b770237b31a429f5f60e7592d24126bfbdaeb5
Ellie Clifford <git@clifford.lol>
2017-11-14 21:29:11 +0000

    Cleanup

--- quicksort.c
+++ quicksort.c
@@ -8,12 +8,14 @@ void quickSort(int[],int,int);
 int main(int argc, char** argv)
 {
 	//srand(time(NULL));
-	const int LENGTH = 1000000;
-	int testArray[LENGTH];// = {0,1,2,3,4,6,5};
+	const int LENGTH = 10000000;
+	printf("ARRAY LENGTH: %d\n",LENGTH);
+	int* testArray = malloc(sizeof(int) * LENGTH + 1);
 	for (int i = 0; i < LENGTH; i++) testArray[i] = rand();
 	quickSort(testArray, 0, LENGTH - 1);
-	//for (int i = 0; i < LENGTH; i++) printf("%d\n",testArray[i]);
+	//for (int i = 0; i < LENGTH; i++) printf("%d\n",testArray[i]); 
 	//printf("\n");
+	printf("SORTED\n");
 }
 void quickSort(int array[], int l, int r)
 {
@@ -23,15 +25,14 @@ void quickSort(int array[], int l, int r)
 	int *data = malloc(sizeof(int) * (r - l + 1));
 	int pivot = array[l];
 	int lCount = 0;
-	int rCount = r - l;
+	int rCount = r - l + 1;
 	for (int i = l + 1; i <= r; i++) {
 		if (array[i] < pivot) data[lCount++] = array[i];
-		else data[rCount--] = array[i];
+		else data[--rCount] = array[i];
 	}
 	data[lCount] = pivot;
 	memcpy(array + l, data, sizeof(int) * (r - l + 1));
-	//free(data);
-	//printf("postsort "); for (int i = 0; i <= (r-l); i++) printf("%d ",array[l+i]); printf("\n");
-	quickSort(array, l, lCount + l); quickSort(array, rCount + l + 1, r);
+	free(data);
+	quickSort(array, l, lCount + l); quickSort(array, rCount + l, r);
 	return;
 }
