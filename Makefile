22b8aa776a624f3ff210e27f6d1fd90237e68aad
Ellie Clifford <git@clifford.lol>
2017-11-21 17:27:51 +0000

    Cleaned up some more

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-all:
-	make quicksort
-CFLAGS=-Wall -g
+debug:
+	gcc -Wall -g quicksort.c -o quicksort
+fast:
+	gcc -Wall -Ofast quicksort.c -o quicksort
 clean:
 	rm -f quicksort
--- quicksort.c
+++ quicksort.c
@@ -5,16 +5,37 @@
 #include <limits.h>
 
 void quickSort(int[],int,int);
+//int mo3pivot(int[],int,int);
+void randomTest(int);
+void reverseTest(int);
 int main(int argc, char** argv)
 {
-	//srand(time(NULL));
+	srand(time(NULL));
 	const int LENGTH = 10000000;
-	printf("ARRAY LENGTH: %d\n",LENGTH);
+	randomTest(LENGTH);
+	//reverseTest(LENGTH);
+
+//	int* testArray = malloc(sizeof(int) * LENGTH + 1);
+//	for (int i = 0; i < LENGTH; i++) testArray[i] = rand();
+//	printf("%d\n",mo3pivot(testArray,0,LENGTH - 1));
+}
+void randomTest(int LENGTH) 
+{
+
+	printf("RANDOM TEST - ARRAY LENGTH: %d\n",LENGTH);
 	int* testArray = malloc(sizeof(int) * LENGTH + 1);
 	for (int i = 0; i < LENGTH; i++) testArray[i] = rand();
 	quickSort(testArray, 0, LENGTH - 1);
 	//for (int i = 0; i < LENGTH; i++) printf("%d\n",testArray[i]); 
-	//printf("\n");
+	printf("SORTED\n");
+}
+void reverseTest(int LENGTH)
+{
+	printf("REVERSE TEST - ARRAY LENGTH: %d\n",LENGTH);
+	int* testArray = malloc(sizeof(int) * LENGTH + 1);
+	for (int i = 0; i < LENGTH; i++) testArray[i] = LENGTH - i;
+	quickSort(testArray, 0, LENGTH - 1);
+	//for (int i = 0; i < LENGTH; i++) printf("%d\n",testArray[i]); 
 	printf("SORTED\n");
 }
 void quickSort(int array[], int l, int r)
@@ -24,6 +45,7 @@ void quickSort(int array[], int l, int r)
 	if (r - l < 1) return;
 	int *data = malloc(sizeof(int) * (r - l + 1));
 	int pivot = array[l];
+	//int pivot = array[mo3pivot(array,l,r)];
 	int lCount = 0;
 	int rCount = r - l + 1;
 	for (int i = l + 1; i <= r; i++) {
@@ -36,3 +58,21 @@ void quickSort(int array[], int l, int r)
 	quickSort(array, l, lCount + l); quickSort(array, rCount + l, r);
 	return;
 }
+/*
+int mo3pivot(int array[], int l, int r) 
+{
+	int first = array[l];
+	int middle = array[(l+r)/2];
+	int last = array[r];
+	printf("%d %d %d %d %d %d",l,(l+r)/2,r,first,middle,last);
+	if (first < middle) {
+		if (middle <= last) return (l+r)/2;
+		else if (last <= first) return l;
+	}
+	else {
+		if (last >= first) return l;
+		else if (middle >= last) return (l+r)/2;
+		return r;
+	}
+}
+*/
